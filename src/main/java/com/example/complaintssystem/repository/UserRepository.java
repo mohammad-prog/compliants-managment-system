47632678fa05dae2e78811e0caa87be45ba62574
mohamamd alqaisi <mohammad.alqaisi@progressoft.io>
2021-07-12 12:42:41 +0300

    A

--- src/main/java/com/example/complaintssystem/repository/UserRepository.java
+++ src/main/java/com/example/complaintssystem/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.example.complaintssystem.repository;
 import com.example.complaintssystem.User;
 
 public interface UserRepository {
-//
+
     void registration(User user);
 
     boolean login(User user);
